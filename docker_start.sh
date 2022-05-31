a35ba1c05dbea06fba935349762bebef743f360e
aionescu <andradenis.ionescu@gmail.com>
2022-05-31 13:26:26 +0200

    Updated docker start file

--- docker_start.sh
+++ docker_start.sh
@@ -2,6 +2,6 @@ if [ ! -f "./.env" ]; then
     echo "No env file found, copying default"
     cp ./.env-default ./.env
 fi
-docker-compose down -v --remove-orphans
+#docker-compose down -v --remove-orphans
 docker-compose build
 docker-compose up
