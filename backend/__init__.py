4664feca2fd390d23af4f1f146bd92518352aca6
Shaad Alaka <shaad1@live.nl>
2022-06-13 09:53:07 +0200

    Add Swagger and some documentation for the API

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,11 +3,15 @@ import os
 from celery import Celery
 from flask import Flask
 from flask_cors import CORS
+from flask_restx import Api
 
 # Flask configuration
 app = Flask(__name__)
 app.debug = True
 app.secret_key = os.urandom(24)
+
+# Swagger configuration
+api = Api(app)
 
 # Celery configuration
 celery = Celery(app.name, broker_url=f"amqp://{os.environ['RABBITMQ_DEFAULT_USER']}:"
