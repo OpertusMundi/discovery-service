072c9cbb216033a83429114ad4b68b87c42ac37f
Shaad Alaka <shaad1@live.nl>
2022-08-31 00:45:06 +0200

    mongo_tools: Add queries for celery and add type annotations

--- backend/search/mongo_tools.py
+++ backend/search/mongo_tools.py
@@ -1,23 +1,40 @@
 from ..clients import mongodb
+from typing import Any, TypedDict
+from pymongo.database import Database
 
 
-def get_db():
+class Table(TypedDict):
+    "name": str
+    "column_count": int
+    "nodes": dict[str, str]
+
+
+def get_db() -> Database:
 	return mongodb.get_client()["db"]
 
 
-def add_table(table_name, column_count, nodes):
+def store_celery_task_id(parent_id: str, task_id: str):
+	get_db().celery_tasks.insert_one({"parent_id": parent_id, "task_id": task_id})
+
+
+def get_celery_task_id(parent_id: str) -> str:
+	res = get_db().celery_tasks.find_one({"parent_id": parent_id})
+	return res["task_id"] if res else None
+
+
+def add_table(table_name: str, column_count: int, nodes: dict[str, str]):
 	get_db().table_metadata.insert_one({"name": table_name, "column_count": column_count, "nodes": nodes})
 
 
-def list_tables():
+def list_tables() -> list[Table]:
 	return list(get_db().table_metadata.find())
 
 
-def get_table(table_name):
+def get_table(table_name: str) -> Table:
 	return get_db().table_metadata.find_one({"name": table_name})
 
 
-def get_node_ids(table_name):
+def get_node_ids(table_name: str) -> dict[str, str]:
 	return get_table(table_name)["nodes"]
 
 
