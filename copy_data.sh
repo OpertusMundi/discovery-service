ae14a12206f608f874e0378016c3d87de8876fc1
Shaad <shaad1@live.nl>
2022-10-27 16:54:47 +0200

    Copy files to the correct volume

--- copy_data.sh
+++ copy_data.sh
@@ -2,6 +2,6 @@
 
 # This is used to copy over data for development purposes
 # Make sure to put your tables in ./data
-docker container create --name temp -v data:/data hello-world
+docker container create --name temp -v discovery-service-fork_data:/data hello-world
 docker cp ./data temp:/data
 docker rm temp
