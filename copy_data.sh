927f40b58623d36f71385e1d865e37d7283d1f87
Shaad <shaad1@live.nl>
2022-11-09 11:00:05 +0100

    Fix copy data script (for testing out production)

--- copy_data.sh
+++ copy_data.sh
@@ -3,5 +3,5 @@
 # This is used to copy over data for development purposes
 # Make sure to put your tables in ./data
 docker container create --name temp -v discovery-service-fork_data:/data hello-world
-docker cp ./data temp:/data
+docker cp ./data temp:/
 docker rm temp
