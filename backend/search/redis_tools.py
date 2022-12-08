d2527c35d63d21b1a607703c76bd848da80325f5
Shaad <shaad1@live.nl>
2022-12-08 13:54:00 +0100

    Deterministically hash path and use that as querying identifier instead

--- backend/search/redis_tools.py
+++ backend/search/redis_tools.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import hashlib
 
 from ast import literal_eval
 # Typing
@@ -13,16 +14,22 @@ from ..clients import redis
 from ..utility.typing import Table
 
 
+def _deterministic_hash(string: str) -> str:
+    return hashlib.sha256(str.encode(string)).hexdigest()
+
+
 def save_celery_task(task_id: str, task_tuple: str) -> None:
     """
     Saves a Celery task as a tuple tree generated from 'as_tuple' in the database under the given task_id.
     """
+    task_id_hash = _deterministic_hash(task_id)
     redis.get_client().json().set(
-        f"task:{task_id.replace('-', '')}",  # See: https://forum.redis.com/t/query-with-dash-is-treated-as-negation/119/12
+        task_id_hash,
         Path.root_path(),
         {
             "task": {
-                "id": task_id.replace('-', ''),
+                "id": task_id_hash,
+                "name": task_id,
                 "task_tuple": str(task_tuple)
             }
         }
@@ -33,7 +40,7 @@ def get_celery_task(task_id: str) -> Optional[tuple]:
     """
     Gets a Celery task tuple tree if the task exists, otherwise returns None.
     """
-    query = Query(f"@id:{task_id.replace('-', '')}")
+    query = Query(f"@id:{_deterministic_hash(task_id)}")
     res = redis.get_client().ft(index_name="task").search(query)
     first = None
     if res.docs:
@@ -45,11 +52,13 @@ def add_table(table_name: str, table_path: str, column_count: int, nodes: Dict[s
     """
     Adds a table with some useful metadata to the database.
     """
+    table_path_hash = _deterministic_hash(table_path)
     redis.get_client().json().set(
-        f"table:{table_path}",
+        f"table:{table_path_hash}",
         Path.root_path(),
         {
             "table": {
+                "id": table_path_hash,
                 "path": table_path,
                 "name": table_name,
                 "column_count": column_count,
@@ -72,7 +81,8 @@ def get_table(table_path: str) -> Optional[Table]:
     """
     Gets table metadata given the table path, or None if nothing was found.
     """
-    query = Query(f"@path:{table_path}")
+    table_path_hash = _deterministic_hash(table_path)
+    query = Query(f"@id:{table_path_hash}")
     res = redis.get_client().ft(index_name="table").search(query)
     first = None
     if res.docs:
